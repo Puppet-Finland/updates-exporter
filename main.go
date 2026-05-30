8297c6ac36008f7824de3e9cb0d2a61f7d07d9e9
Ricky Cousins <rdcuzins@gmail.com>
2026-05-30 14:47:35 +0300

    Switch to viper and pflag

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -15,6 +19,13 @@ import (
 	"github.com/Puppet-Finland/updates-exporter/distros/rhel"
 	"github.com/Puppet-Finland/updates-exporter/distros/ubuntu"
 )
+
+type Config struct {
+	Port     int  `mapstructure:"port"`
+	Interval int  `mapstructure:"interval"`
+	Version  bool `mapstructure:"version"`
+	Debug    bool `mapstructure:"debug"`
+}
 
 const (
 	DEFAULT_INTERVAL = 3600
@@ -64,15 +75,23 @@ func updateMetrics(d distros.Distro) {
 }
 
 func main() {
-	port := flag.Int("port", DEFAULT_PORT, "HTTP port")
-	interval := flag.Int("interval", DEFAULT_INTERVAL, "Metrics refresh interval (seconds)")
-	versionFlag := flag.Bool("v", false, "Print version")
+	cfg, err := loadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
 
-	flag.Parse()
-
-	if *versionFlag {
-		fmt.Printf("Updates-Exporter Version: %s\n", Version)
+	if cfg.Version {
+		fmt.Println(Version)
 		os.Exit(0)
+	}
+
+	if cfg.Debug {
+		cfgBytes, err := json.Marshal(cfg)
+		if err != nil {
+			log.Fatalf("Failed to marshal config for debugging: %v", err)
+		}
+
+		log.Printf("Starting with: %s", string(cfgBytes))
 	}
 
 	prometheus.MustRegister(securityUpdates)
@@ -87,11 +106,45 @@ func main() {
 	go func() {
 		for {
 			updateMetrics(distro)
-			time.Sleep(time.Duration(*interval) * time.Second)
+			time.Sleep(time.Duration(cfg.Interval) * time.Second)
 		}
 	}()
 
 	http.Handle("/metrics", promhttp.Handler())
-	fmt.Printf("Starting updates_exporter on :%d, updating every %d seconds\n", *port, *interval)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+	fmt.Printf("Starting updates_exporter on :%d, updating every %d seconds\n", cfg.Port, cfg.Interval)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), nil))
+}
+
+func loadConfig() (*Config, error) {
+	pflag.IntP("port", "p", DEFAULT_PORT, "HTTP port")
+	pflag.IntP("interval", "i", DEFAULT_INTERVAL, "Metrics refresh interval (seconds)")
+	pflag.BoolP("version", "v", false, "Print version")
+	pflag.BoolP("debug", "d", false, "Enables Debug messages")
+
+	pflag.Parse()
+
+	if err := viper.BindPFlags(pflag.CommandLine); err != nil {
+		return nil, fmt.Errorf("unable to bind flags: %w", err)
+	}
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".") // Look for the file in the current working directory
+	viper.AddConfigPath("/etc/updates-exporter/")
+
+	if err := viper.ReadInConfig(); err != nil {
+		// It is acceptable if the config file is missing; we fall back to flags/defaults.
+		// But if it exists and has a syntax error, we want to fail fast.
+		var configFileNotFoundError viper.ConfigFileNotFoundError
+		if !errors.As(err, &configFileNotFoundError) {
+			return nil, fmt.Errorf("error reading config file: %w", err)
+		}
+	}
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, fmt.Errorf("unable to decode into struct: %w", err)
+	}
+
+	return &cfg, nil
 }
